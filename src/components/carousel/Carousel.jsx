9c5b3cac0389c46ba7862a1b0b4181bc96bd6e24
Felipao22 <felipeaviani@gmail.com>
2023-10-19 12:45:09 -0300

    Agregué imagen de Pro en carousel

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -5,18 +5,15 @@ import logoBlanco from '../../assets/LOGO CUADRADO blanco.webp';
 import exit from '../../assets/exit.webp';
 import im1 from '../../assets/im1.webp';
 import im3 from '../../assets/im3.webp'
+import pro from '../../assets/PRO.webp'
 
 export default function Carrusel() {
   return (
     <Carousel interval={3000} >
       <Carousel.Item>
-        <img className="img-overlay" src={logoBlanco} alt="log" />
-        <div className="box-text">
-      <h2 className="text-overlay">Gestión y Asesoramiento en Higiene y Seguridad en el Trabajo.</h2>
-        </div>
         <img
           className="d-block w-100"
-          src={exit}
+          src={pro}
           alt="First slide"
         />
       </Carousel.Item>
@@ -27,7 +24,7 @@ export default function Carrusel() {
         </div>
         <img
           className="d-block w-100"
-          src={im3}
+          src={exit}
           alt="Second slide"
         />
       </Carousel.Item>
